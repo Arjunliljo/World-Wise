646e5757ba7129c1aa6669585e0b4b3e13b0919c
Arjun cv <arjun7180@gmail.com>
2024-06-11 14:50:29 +0530

    Made use reducer for concise code

--- src/Components/Contexts/CityContext.jsx
+++ src/Components/Contexts/CityContext.jsx
@@ -1,19 +1,94 @@
-import { createContext, useContext, useEffect, useState } from "react";
+import {
+  createContext,
+  useContext,
+  useEffect,
+  useReducer,
+  useState,
+} from "react";
 
 const BASE_URL = "http://localhost:8000";
 const CityContext = createContext();
 
-function CityProvider({ children }) {
-  const [cities, setCities] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const [currentCity, setCurrentCity] = useState("");
+const initialState = {
+  cities: [],
+  isLoading: false,
+  currentCity: "",
+  isChecked: false,
+  isMobile: window.innerWidth <= 800,
+  error: "",
+};
 
-  const [isChecked, setIsChecked] = useState(false);
-  const [isMobile, setIsMobile] = useState(() => window.innerWidth <= 800);
+function reducer(state, action) {
+  switch (action.type) {
+    case "Loading":
+      return {
+        ...state,
+        isLoading: true,
+      };
+    case "checked":
+      return {
+        ...state,
+        isChecked: action.payload,
+      };
+    case "smallScreen":
+      return {
+        ...state,
+        isMobile: action.payload,
+      };
+    case "cities/loaded":
+      return {
+        ...state,
+        cities: action.payload,
+        isLoading: false,
+      };
+
+    case "city/loaded":
+      return {
+        ...state,
+        currentCity: action.payload,
+        isLoading: false,
+      };
+    case "cities/created":
+      return {
+        ...state,
+        isLoading: false,
+        cities: [...state.cities, action.payload],
+        currentCity: action.payload,
+      };
+    case "cities/deleted":
+      return {
+        ...state,
+        cities: state.cities.filter((city) => city.id !== action.payload),
+      };
+    case "rejected":
+      return {
+        ...state,
+        error: action.payload,
+        isLoading: false,
+      };
+  }
+}
+
+function CityProvider({ children }) {
+  // const [cities, setCities] = useState([]);
+  // const [isLoading, setIsLoading] = useState(false);
+  // const [currentCity, setCurrentCity] = useState("");
+
+  // const [isChecked, setIsChecked] = useState(false);
+  // const [isMobile, setIsMobile] = useState(() => window.innerWidth <= 800);
+
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const { cities, isChecked, isLoading, currentCity, isMobile } = state;
+
+  const setIsChecked = (checked) => {
+    dispatch({ type: "navigation", payload: checked });
+  };
 
   useEffect(() => {
     const handleResize = () => {
-      setIsMobile(window.innerWidth <= 800);
+      // setIsMobile(window.innerWidth <= 800);
+      dispatch({ type: "smallScreen", payload: window.innerWidth <= 800 });
     };
 
     window.addEventListener("resize", handleResize);
@@ -24,17 +99,16 @@ function CityProvider({ children }) {
   }, []);
 
   useEffect(() => {
-    setIsLoading(true);
+    dispatch({ type: "loading" });
 
     const fetchCities = async () => {
       try {
         const res = await fetch(`${BASE_URL}/cities`);
         const data = await res.json();
-        setCities(data);
+
+        dispatch({ type: "cities/loaded", payload: data });
       } catch {
-        alert("Error while fetching cities");
-      } finally {
-        setIsLoading(false);
+        dispatch({ type: "rejected", payload: "Erro rWhile fetching" });
       }
     };
 
@@ -44,20 +118,19 @@ function CityProvider({ children }) {
   }, []);
 
   async function getCity(id) {
-    setIsLoading(true);
+    dispatch({ type: "loading" });
 
     try {
       const res = await fetch(`${BASE_URL}/cities/${id}`);
       const data = await res.json();
-      setCurrentCity(data);
+      dispatch({ type: "city/loaded", payload: data });
     } catch {
       alert("Error while fetching cities");
-    } finally {
-      setIsLoading(false);
+      dispatch({ type: "rejected", payload: "Error while fetching Cities" });
     }
   }
   async function createCity(newCity) {
-    setIsLoading(true);
+    dispatch({ type: "loading" });
 
     try {
       const res = await fetch(`${BASE_URL}/cities/`, {
@@ -68,27 +141,26 @@ function CityProvider({ children }) {
         },
       });
       const data = await res.json();
-      setCurrentCity(data);
-
-      setCities((cities) => [...cities, newCity]);
+      // setCurrentCity(data);
+      dispatch({ type: "city/created", payload: newCity });
+      // setCities((cities) => [...cities, newCity]);
     } catch {
+      dispatch({ type: "rejected", payload: "Error while creating city" });
       alert("Error while creating cities");
-    } finally {
-      setIsLoading(false);
     }
   }
   async function deleteCity(id) {
-    setIsLoading(true);
+    dispatch({ type: "loading" });
     try {
       await fetch(`${BASE_URL}/cities/`, {
         method: "DELETE",
       });
 
-      setCities((cities) => cities.filter((city) => city.id !== id));
+      dispatch({ type: "city/delted", payload: id });
     } catch {
       alert("Error while deleting cities");
-    } finally {
-      setIsLoading(false);
+      dispatch({ type: "rejected", payload: "Error while creating city" });
+      alert("Error while creating cities");
     }
   }
 
@@ -103,7 +175,6 @@ function CityProvider({ children }) {
         setIsChecked,
         createCity,
         isMobile,
-        setIsMobile,
         deleteCity,
       }}
     >
