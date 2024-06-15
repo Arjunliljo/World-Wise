adf701e6d64b3ed809e9e48131726b415c9b2768
Arjun cv <arjun7180@gmail.com>
2024-06-15 17:52:26 +0530

    made some minor bug issues

--- src/Components/UtilityComponents/Logo.jsx
+++ src/Components/UtilityComponents/Logo.jsx
@@ -1,9 +1,11 @@
 import { Link } from "react-router-dom";
 import styles from "./Logo.module.css";
+import { useCities } from "../Contexts/CityContext";
 
 function Logo() {
+  const { setIsChecked } = useCities();
   return (
-    <Link to="/">
+    <Link to="/" onClick={() => setIsChecked(false)}>
       <img src="/logo.png" alt="WorldWise logo" className={styles.logo} />
     </Link>
   );
